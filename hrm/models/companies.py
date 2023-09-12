abb57c7f9a9d4e0fe253128232134fdf235c9fe4
Doan <doanvh@doanvh.com>
2023-09-12 20:13:52 +0700

    update function logic name, form view companies

--- hrm/models/companies.py
+++ hrm/models/companies.py
@@ -1,5 +1,4 @@
 from odoo import models, fields, api
-from odoo.exceptions import ValidationError
 from . import constraint
 
 
@@ -7,27 +6,28 @@ class Companies(models.Model):
     _name = "hrm.companies"
     _description = "Companies"
 
-    name = fields.Char(string="Tên hiển thị")
+    name = fields.Char(string="Tên hiển thị", compute="update_name_company", readonly=True)
     name_company = fields.Char(string="Tên Công ty", required=True)
-    parent_company = fields.Many2one('hrm.companies', string="Công ty cha", domain=[])
+    parent_company = fields.Many2one('hrm.companies', string="Công ty cha")
     type_company = fields.Selection(selection=constraint.SELECT_TYPE_COMPANY, string="Loại công ty", required=True)
     system_id = fields.Many2one('hrm.systems', string="Hệ thống", required=True)
     phone_num = fields.Char(string="Số điện thoại", required=True)
     chairperson = fields.Many2one('res.users', string="Chủ tịch")
     vice_president = fields.Many2one('res.users', string='Phó chủ tịch')
 
-    """
-    decorator này để tự động tạo Tiên hiển thị theo logic 'Tiền tố . Tên hệ thông . Tên công ty'
-    """
-    @api.onchange('system_id', 'type_company', 'name_company')
+    @api.depends('system_id', 'type_company', 'name_company')
     def update_name_company(self):
-        for company in self:
+        """
+        decorator này để tự động tạo Tiên hiển thị theo logic 'Tiền tố . Tên hệ thông . Tên công ty'
+        """
+        for rec in self:
             prefix = ""
-            if company.type_company == 'sale':
+            if rec.type_company == 'sale':
                 prefix = 'S'
-            else:
+            elif rec.type_company == 'upsale':
                 prefix = 'U'
-
-            name_display = f"{prefix}.{company.system_id.name}.{company.name_company}"
-
-            company.name = name_display
+            else:
+                rec.name = ""
+                return
+            name_display = f"{prefix}.{rec.system_id.name}.{rec.name_company}"
+            rec.name = name_display
