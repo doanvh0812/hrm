baa829fe04540362ea19648a701383ea57f54891
LeTrung3008 <letrung3008.nguyen@gmail.com>
2023-09-12 15:34:50 +0700

    update

--- hrm/models/position.py
+++ hrm/models/position.py
@@ -1,11 +1,25 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from . import constraint
 
 
 class Position(models.Model):
     _name = 'hrm.position'
 
     work_position = fields.Char(string='Tên Vị Trí', required=True)
-    block = fields.Selection([('commerce', 'Thương Mại'), ('office', 'Văn Phòng')],
-                             default='commerce', required=True)
+    block = fields.Many2one('hrm.blocks', string="Khối", required=True, default=lambda self: self.__default_block_())
+    department = fields.Many2one("hrm.departments", string='Phòng/Ban')
 
-    department = fields.Char(string='HRM-Cấu Hình-Phòng/Ban')
+    related = fields.Boolean()
+
+    @api.onchange('block')
+    def _compute_related_field(self):
+        for record in self:
+            if record.block.name == constraint.BLOCK_OFFICE_NAME:
+                record.related = True
+            else:
+                record.related = False
+
+    def _default_block_(self):
+        ids = self.env['hrm.blocks'].search([('work_position', '=', constraint.BLOCK_TRADE_NAME)], limit=1).id
+        print('---------------->', ids)
+        return ids
