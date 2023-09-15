f3d8d75dff79d5c61169fff1b9c456bd95585151
DWMTVietHung <doviethung998@gmail.com>
2023-09-15 10:17:13 +0700

    update

--- hrm/models/employee_profile.py
+++ hrm/models/employee_profile.py
@@ -16,9 +16,14 @@ class EmployeeProfile(models.Model):
     work_start_date = fields.Date(string='Ngày vào làm')
     date_receipt = fields.Date(string='Ngày được nhận chính thức', required=True, default=datetime.now())
 
-
     employee_code_old = fields.Char(string='Mã nhân viên cũ')
-    employee_code_new = fields.Char(string='Mã nhân viên mới', required=True)
+    employee_code_new = fields.Char(
+        string="Mã nhân viên mới",
+        readonly=True,
+        index=True
+        # ,default=lambda self: self.env['ir.sequence'].next_by_code('hrm.employee.profile')
+    )
+
     email = fields.Char('Email công việc', required=True)
     phone_num = fields.Char('Số điện thoại di động', required=True, max_length=10)
     identifier = fields.Char('Số căn cước công dân', required=True, max_length=10)
@@ -31,18 +36,11 @@ class EmployeeProfile(models.Model):
     manager_id = fields.Many2one('res.users', string='Quản lý')
     rank_id = fields.Char(string='Cấp bậc')
     auto_create_acc = fields.Boolean(string='Tự động tạo tài khoản', default=True)
-    employee_code = fields.Char(
-        string="Mã nhân viên",
-        required=True,
-        copy=False,
-        index=True,
-        size=7,
-        default=lambda self: self._generate_employee_code(),
-    )
+
     # lọc duy nhất mã nhân viên
 
     _sql_constraints = [
-        ('employee_code_uniq', 'unique(employee_code)', 'Mã nhân viên phải là duy nhất!'),
+        ('employee_code_uniq', 'unique(employee_code_new)', 'Mã nhân viên phải là duy nhất!'),
     ]
 
     active = fields.Boolean(string='Hoạt động', default=True)
@@ -52,36 +50,48 @@ class EmployeeProfile(models.Model):
     def _compute_related_(self):
         # Lấy giá trị của trường related để check điều kiện hiển thị
         for record in self:
-            record.related = record.block_id.name != constraint.BLOCK_OFFICE_NAME
+            record.related = record.block_id.name == constraint.BLOCK_OFFICE_NAME
 
     def _default_block_(self):
         # Đặt giá trị mặc định cho Khối
         ids = self.env['hrm.blocks'].search([('name', '=', constraint.BLOCK_COMMERCE_NAME)]).id
         return ids
 
-
-    # hiển thị mã nhân viên, nếu chọn 1 số bất kỳ tiếp tục nhảy tiếp từ số đó
-    @api.model
-    def _generate_employee_code(self):
-        last_employee = self.search([], order='employee_code desc', limit=1)
-        if last_employee:
-            last_employee_code = last_employee.employee_code
-            last_number = int(last_employee_code[3:])  # Trích xuất phần số
-            new_number = last_number + 1
-            new_employee_code = f'UNI{str(new_number).zfill(4)}'
-            return new_employee_code
-        return 'UNI0000'
+        # hiển thị mã nhân viên, nếu chọn 1 số bất kỳ tiếp tục nhảy tiếp từ số đó
+        # @api.onchange('system_id')
+        # def _generate_employee_code(self):
+        # last_employee = self.search([], order='employee_code_new desc', limit=1)
+        # last_employee_code = last_employee.employee_code_new
+        # last_number = int(last_employee_code[3:])  # Trích xuất phần số
+        # new_number = last_number + 1
+        # print('-----------', self.get_name_systems())
+        # new_employee_code = f'UNI{str(new_number).zfill(4)}'
+        # return new_employee_code
+        # return 'UNI0001'
 
     # Lọc nhân viên
-    @api.constrains('employee_code')
-    def check_employee_code_format(self):
-        for record in self:
-            if record.employee_code and not record.employee_code.startswith('UNI'):
-                raise ValidationError(_("Mã nhân viên phải bắt đầu bằng 'UNI'."))
+    # @api.constrains('employee_code_new')
+    # def check_employee_code_format(self):
+    #     for record in self:
+    #         if record.employee_code and not record.employee_code.startswith('UNI'):
+    #             raise ValidationError(_("Mã nhân viên phải bắt đầu bằng 'UNI'."))
 
+    #
 
-
-
+    # @api.depends('system_id')
+    # def render_code(self):
+    #     for rec in self:
+    #         if rec.system_id:
+    #             name = str.split(rec.system_id.name, '.')[0]
+    #             last_employee_code = self.env['hrm.employee.profile'].search([('employee_code_new', 'like', name)],
+    #                                                                          order='employee_code_new desc',
+    #                                                                          limit=1).employee_code_new
+    #             if last_employee_code:
+    #                 numbers = int(re.search(r'\d+', last_employee_code).group(0)) + 1
+    #                 rec.employee_code_new = name + str(numbers).zfill(4)
+    #                 print(rec.employee_code_new)
+    #             else:
+    #                 rec.employee_code_new = str.upper(name) + '0001'
 
     """decorator này tạo hồ sơ nhân viên, chọn cty cho hồ sơ đó 
     sẽ tự hiển thị hệ thống mà công ty đó thuộc vào"""
@@ -90,7 +100,6 @@ class EmployeeProfile(models.Model):
         decorator này tạo hồ sơ nhân viên, chọn cty cho hồ sơ đó 
         sẽ tự hiển thị hệ thống mà công ty đó thuộc vào
     """
-
 
     @api.onchange('company')
     def _onchange_company(self):
@@ -134,11 +143,13 @@ class EmployeeProfile(models.Model):
                 if not re.match(r'^\d+$', rec.identifier):
                     raise ValidationError("Số căn cước công dân không hợp lệ")
 
-    
-
-
-
-
-
-
-
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get('block_id') == 1:
+                sequence_code = 'hrm.employee.profile'
+            else:
+                break
+            sequence = self.env['ir.sequence'].sudo().next_by_code(sequence_code)
+            vals['employee_code_new'] = sequence or _('New')
+        return super(EmployeeProfile, self).create(vals_list)
