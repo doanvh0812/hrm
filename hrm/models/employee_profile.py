5563e91980f8205c676a94852164ad0114d66170
LeTrung3008 <letrung3008.nguyen@gmail.com>
2023-09-20 17:03:04 +0700

    update

--- hrm/models/employee_profile.py
+++ hrm/models/employee_profile.py
@@ -14,7 +14,7 @@ class EmployeeProfile(models.Model):
     block_id = fields.Many2one('hrm.blocks', string='Khối', required=True, default=lambda self: self._default_block_(),
                                tracking=True)
     position_id = fields.Many2one('hrm.position', required=True, string='Vị trí', tracking=True)
-    work_start_date = fields.Date(string='Ngày vào làm',tracking=True)
+    work_start_date = fields.Date(string='Ngày vào làm', tracking=True)
     employee_code_old = fields.Char(string='Mã nhân viên cũ')
     employee_code_new = fields.Char(
         string="Mã nhân viên mới",
@@ -22,17 +22,17 @@ class EmployeeProfile(models.Model):
         store=True
     )
 
-    email = fields.Char('Email công việc', required=True,tracking=True)
+    email = fields.Char('Email công việc', required=True, tracking=True)
     phone_num = fields.Char('Số điện thoại di động', required=True, tracking=True)
     identifier = fields.Char('Số căn cước công dân', required=True)
     profile_status = fields.Selection(constraint.PROFILE_STATUS, string='Trạng thái hồ sơ', default='incomplete',
                                       tracking=True)
     system_id = fields.Many2one('hrm.systems', string='Hệ thống', tracking=True)
     company = fields.Many2one('hrm.companies', string='Công ty con', tracking=True)
-    team_marketing = fields.Char(string='Đội ngũ marketing',tracking=True)
-    team_sales = fields.Char(string='Đội ngũ bán hàng',tracking=True)
+    team_marketing = fields.Char(string='Đội ngũ marketing', tracking=True)
+    team_sales = fields.Char(string='Đội ngũ bán hàng', tracking=True)
     department_id = fields.Many2one('hrm.departments', string='Phòng/Ban', tracking=True)
-    manager_id = fields.Many2one('res.users', string='Quản lý',tracking=True)
+    manager_id = fields.Many2one('res.users', string='Quản lý', tracking=True)
     rank_id = fields.Char(string='Cấp bậc')
     auto_create_acc = fields.Boolean(string='Tự động tạo tài khoản', default=True)
 
@@ -291,59 +291,92 @@ class EmployeeProfile(models.Model):
         #     'state': 'pending'
         # })
 
-
     def process_block(self):
         orders = self.filtered(lambda s: s.state in ['draft'])
         records = self.env['hrm.approval.flow.object'].search([])
-        list_department = [record.department_id for record in records]
-
+        list_dept = []
         name_department = []
+        self._cr.execute(
+            'SELECT * FROM hrm_departments d JOIN hrm_departments d1 ON d.superior_department = d1.id ORDER BY d.superior_department ASC')
+
+        for item in self._cr.fetchall():
+            list_dept.append(item[1])
+
         for rec in records:
             for dept in rec.department_id:
                 if dept:
-                    name_department.append(dept)
+                    name_department.append(dept.name)
 
-        for i in name_department:
-            print(i.name)
+        print(list_dept)
+        print(name_department)
 
-        # if not self.check_department(self.department_id, list_department):
-        #     if not self.check_department(self.department_id.superior_department, list_department):
-        #         blocks = self.env['hrm.approval.flow.object'].search([('block_id', '=', self.block_id.id)])
-        #         if not blocks:
-        #             raise ValidationError("LỖI KHÔNG TÌM THẤY LUỒNG")
-        #             return
-        #         else:
-        #             approved_id = self.env['hrm.approval.flow.object'].search([('block_id', '=', self.block_id.id)])
-        #             print('Lấy theo khối')
-        #     else:
-        #         approved_id = self.env['hrm.approval.flow.object'].search(
-        #             [('department_id', '=', self.department_id.superior_department.id)])
-        #         print('Lấy theo phòng ban cha')
-        # else:
-        #     approved_id = self.env['hrm.approval.flow.object'].search([('department_id', '=', self.department_id.id)])
-        #     print('Lấy theo phòng ban con')
+    def check_duplicate_stream(self, list1, list2):
+        for i in list1:
+            for y in list2:
+                if i in y:
+                    return i
+            return None
 
-    def check_department(self, department, list_department):
-        for rec in list_department:
-            if rec:
-                if department in rec:
-                    return True
+    #     self._cr.execute(
+    #         'select * from hrm_companies where hrm_companies.system_id in %s;',
+    #         (tuple(list_systems_id),))
+    #     list_systems_id.clear()
+    #     for item in self._cr.fetchall():
+    #         list_systems_id.append(item[0])
+    #     return {'domain': {'company': [('id', 'in', list_systems_id)]}}
+    # else:
+    #     return {'domain': {'company': []}}
+    # name_department = []
+    # for rec in records:
+    #     for dept in rec.department_id:
+    #         if dept:
+    #             name_department.append(dept)
+    #
+    # for i in name_department:
+    #     print(i.name)
 
-    def name_configure(self, lst, list2):
-        ...
+    # if not self.check_department(self.department_id, list_department):
+    #     if not self.check_department(self.department_id.superior_department, list_department):
+    #         blocks = self.env['hrm.approval.flow.object'].search([('block_id', '=', self.block_id.id)])
+    #         if not blocks:
+    #             raise ValidationError("LỖI KHÔNG TÌM THẤY LUỒNG")
+    #             return
+    #         else:
+    #             approved_id = self.env['hrm.approval.flow.object'].search([('block_id', '=', self.block_id.id)])
+    #             print('Lấy theo khối')
+    #     else:
+    #         approved_id = self.env['hrm.approval.flow.object'].search(
+    #             [('department_id', '=', self.department_id.superior_department.id)])
+    #         print('Lấy theo phòng ban cha')
+    # else:
+    #     approved_id = self.env['hrm.approval.flow.object'].search([('department_id', '=', self.department_id.id)])
+    #     print('Lấy theo phòng ban con')
 
-    def find_common_elements(self, list1, list2):
-        for i in range(len(list1) - 1, 0, -1):
-            for str2 in list2:
-                if list1[i] in str2:
-                    return list1[i]
-        return None
 
-    # hàm này để hiển thị lịch sử lưu trữ
-    def toggle_active(self):
-        for record in self:
-            record.active = not record.active
-            if not record.active:
-                record.message_post(body="Đã lưu trữ")
-            else:
-                record.message_post(body="Bỏ lưu trữ")
+def check_department(self, department, list_department):
+    for rec in list_department:
+        if rec:
+            if department in rec:
+                return True
+
+
+def name_configure(self, lst, list2):
+    ...
+
+
+def find_common_elements(self, list1, list2):
+    for i in range(len(list1) - 1, 0, -1):
+        for str2 in list2:
+            if list1[i] in str2:
+                return list1[i]
+    return None
+
+
+# hàm này để hiển thị lịch sử lưu trữ
+def toggle_active(self):
+    for record in self:
+        record.active = not record.active
+        if not record.active:
+            record.message_post(body="Đã lưu trữ")
+        else:
+            record.message_post(body="Bỏ lưu trữ")
