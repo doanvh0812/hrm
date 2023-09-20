ff6451c4f5d045a37463aeac61f8730977cd16cd
tienkmt21 <khongtien261@gmail.com>
2023-09-20 11:11:41 +0700

    fix bug 20/09

--- hrm/models/employee_profile.py
+++ hrm/models/employee_profile.py
@@ -1,6 +1,4 @@
 import re
-from datetime import datetime
-
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 from . import constraint
@@ -14,7 +12,7 @@ class EmployeeProfile(models.Model):
     block_id = fields.Many2one('hrm.blocks', string='Khối', required=True, default=lambda self: self._default_block_())
     position_id = fields.Many2one('hrm.position', required=True, string='Vị trí')
     work_start_date = fields.Date(string='Ngày vào làm')
-    date_receipt = fields.Date(string='Ngày được nhận chính thức', required=True, default=datetime.now())
+    date_receipt = fields.Date(string='Ngày được nhận chính thức', required=True, default=fields.Datetime.now())
 
     employee_code_old = fields.Char(string='Mã nhân viên cũ')
     employee_code_new = fields.Char(
@@ -47,6 +45,7 @@ class EmployeeProfile(models.Model):
 
     # Các trường trong tab
     approved_link = fields.One2many('hrm.approval.flow.profile', 'profile_id')
+    approved_name = fields.Many2one('hrm.approval.flow.object')
 
     @api.depends('system_id', 'block_id')
     def render_code(self):
@@ -74,7 +73,7 @@ class EmployeeProfile(models.Model):
             query dữ liệu từ dưới lên gặp mã nào trùng thì lấy và kết thúc query
             Kết quả cuối cùng return về mã nhân viên nếu có hoặc None nếu không thấy
         """
-        domain = [('employee_code_new', operator, name)]
+        domain = [('employee_code_new', operator, name), ('active', 'in', (True, False))]
         order = 'employee_code_new desc'
         limit = 1
         last_employee = self.env['hrm.employee.profile'].search(domain, order=order, limit=limit)
@@ -106,10 +105,8 @@ class EmployeeProfile(models.Model):
 
     @api.onchange('company')
     def _onchange_company(self):
-        """
-            decorator này tạo hồ sơ nhân viên, chọn cty cho hồ sơ đó
-            sẽ tự hiển thị hệ thống mà công ty đó thuộc vào
-        """
+        """decorator này tạo hồ sơ nhân viên, chọn cty cho hồ sơ đó
+             sẽ tự hiển thị hệ thống mà công ty đó thuộc vào"""
         if self.company:
             company_system = self.company.system_id
             if company_system:
@@ -119,24 +116,37 @@ class EmployeeProfile(models.Model):
 
     @api.onchange('system_id')
     def _onchange_system_id(self):
-        """
-            decorator này khi tạo hồ sơ nhân viên, chọn 1 hệ thống nào đó
-            khi ta chọn công ty nó sẽ hiện ra tất cả những công ty có trong hệ thống đó
-        """
+        """ decorator này khi tạo hồ sơ nhân viên, chọn 1 hệ thống nào đó
+            khi ta chọn cty nó sẽ hiện ra tất cả những cty có trong hệ thống đó
+            """
+        # clear dữ liệu
+        if self.system_id != self.company.system_id:
+            self.position_id = self.company = self.team_sales = self.team_marketing = False
+
         if self.system_id:
-            companies = self.env['hrm.companies'].search([('block_id', '=', self.system_id.id)])
-            return {'domain': {'company': [('id', 'in', companies.ids)]}}
+            list_systems_id = []
+            self._cr.execute(
+                'select * from hrm_systems as hrm1 left join hrm_systems as hrm2 on hrm2.parent_system = hrm1.id where hrm1.name ILIKE %s;',
+                (self.system_id.name + '%',))
+            for item in self._cr.fetchall():
+                list_systems_id.append(item[0])
+            self._cr.execute(
+                'select * from hrm_companies where hrm_companies.system_id in %s;',
+                (tuple(list_systems_id),))
+            list_systems_id.clear()
+            for item in self._cr.fetchall():
+                list_systems_id.append(item[0])
+            return {'domain': {'company': [('id', 'in', list_systems_id)]}}
         else:
             return {'domain': {'company': []}}
 
     @api.onchange('block_id')
-    def _onchange_system_id(self):
+    def _onchange_block_id(self):
         """
             decorator này khi tạo hồ sơ nhân viên, chọn 1 vị trí nào đó
             khi ta vị trí nó sẽ hiện ra tất cả những vị trí có trong khối đó
         """
-        self.position_id = self.system_id = self.company = self.team_sales = self.team_marketing = self.department_id = self.manager_id = self.rank_id = ''
-
+        self.position_id = self.system_id = self.company = self.team_sales = self.team_marketing = self.department_id = self.manager_id = self.rank_id = False
         if self.block_id:
             position = self.env['hrm.position'].search([('block', '=', self.block_id.name)])
             return {'domain': {'position_id': [('id', 'in', position.ids)]}}
@@ -150,7 +160,7 @@ class EmployeeProfile(models.Model):
         """
         for rec in self:
             if rec.phone_num:
-                if not re.match(r'^[0]\d+$', rec.phone_num):
+                if not re.match(r'^\d+$', rec.phone_num):
                     raise ValidationError(constraint.ERROR_PHONE)
 
     @api.constrains("identifier")
@@ -221,10 +231,50 @@ class EmployeeProfile(models.Model):
     def action_send(self):
         # Khi ấn button Gửi duyệt sẽ chuyển từ draft sang pending
         orders = self.filtered(lambda s: s.state in ['draft'])
-        # Tìm khối trùng với khối của hồ sơ
-        approved_id = self.env['hrm.approval.flow.object'].search([('block_id.name', 'like', orders.block_id.name)])
+        # Tìm công ty trùng với công ty của hồ sơ
+        records = self.env['hrm.approval.flow.object'].search([])
 
+        # Lấy tên tất cả công ty, hệ thống được cấu hình
+        name_company_configured = []
+        name_system_configured = []
+        for rec in records:
+            for sys in rec.system_id:
+                if sys:
+                    name_system_configured.append(sys.name_system)
+            for comp in rec.company:
+                if comp:
+                    name_company_configured.append(comp.name_company)
+
+        # Lấy tên hệ thống, công ty trong quan hệ cha con
+        list_name_system = self.system_id.name.split('.')
+        list_name_company = self.company.name.split('.')
+        system_last = self.find_common_elements(list_name_system[2:], name_system_configured)
+        company_last = self.find_common_elements(list_name_company, name_company_configured)
+        approval_list = self.env['hrm.approval.flow.object'].search([])
+        if company_last:
+            # Duyệt qua tất cả bản ghi
+            for rec in approval_list:
+                list_name = []
+                for i in rec.company:
+                    list_name.append(rec.company.name_company)
+                if company_last in list_name:
+                    approved_id = rec
+
+        elif system_last:
+            for rec in approval_list:
+                list_name = []
+                for i in rec.company:
+                    list_name.append(rec.company.name_company)
+                if system_last in list_name:
+                    approved_id = rec
+        else:
+            approved_id = self.env['hrm.approval.flow.object'].search([('block_id', '=', self.block_id.id)])
+
+        if not approved_id:
+            raise ValidationError("LỖI KHÔNG TÌM THẤY LUỒNG")
+        print(approved_id.id)
         if approved_id:
+            self.approved_name = approved_id.id
             for rec in approved_id.approval_flow_link:
                 self.approved_link.create({
                     'profile_id': self.id,
@@ -236,6 +286,13 @@ class EmployeeProfile(models.Model):
                     'time': False,
                 })
 
-        return orders.write({
-            'state': 'pending'
-        })
+        # return orders.write({
+        #     'state': 'pending'
+        # })
+
+    def find_common_elements(self, list1, list2):
+        for i in range(len(list1) - 1, 0, -1):
+            for str2 in list2:
+                if list1[i] in str2:
+                    return list1[i]
+        return None
