e4752422a7118459be10ceaa80179e10eb6625c9
Doan <doanvh@doanvh.com>
2023-09-25 10:58:04 +0700

    format file

--- hrm/models/systems.py
+++ hrm/models/systems.py
@@ -20,7 +20,6 @@ class Systems(models.Model):
     company_ids = fields.One2many('hrm.companies', 'system_id', string='Công ty trong hệ thống')
     approval_id = fields.Many2one('hrm.approval.flow.object', tracking=True)
 
-
     @api.depends("parent_system.name", "name_system")
     def _compute_name(self):
         """ Tính toán logic tên hiển thị """
