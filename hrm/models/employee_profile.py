f997cbe361c8ff4b3cf21879b8aeb304055ae307
tienkmt21 <khongtien261@gmail.com>
2023-09-29 13:13:25 +0700

    update

--- hrm/models/employee_profile.py
+++ hrm/models/employee_profile.py
@@ -2,7 +2,8 @@ from odoo import models, fields, api, _
 import re
 from odoo.exceptions import ValidationError
 from . import constraint
-
+from lxml import etree
+import json
 
 class EmployeeProfile(models.Model):
     _name = 'hrm.employee.profile'
@@ -12,7 +13,8 @@ class EmployeeProfile(models.Model):
     date_receipt = fields.Date(string='Ngày được nhận chính thức', required=True,
                                default=lambda self: self._get_server_date())
     name = fields.Char(string='Họ và tên nhân sự', required=True, tracking=True)
-    block_id = fields.Many2one('hrm.blocks', string='Khối', required=True, default=lambda self: self._default_block_(),
+    block_id = fields.Many2one('hrm.blocks', string='Khối', required=True,
+                               default=lambda self: self.env['hrm.utils'].default_block_(),
                                tracking=True)
     position_id = fields.Many2one('hrm.position', required=True, string='Vị trí', tracking=True)
     work_start_date = fields.Date(string='Ngày vào làm', tracking=True)
@@ -22,7 +24,7 @@ class EmployeeProfile(models.Model):
         compute='render_code',
         store=True
     )
-
+    # list_fields = ['name', 'phone_num', 'email', '', '']
     email = fields.Char('Email công việc', required=True, tracking=True)
     phone_num = fields.Char('Số điện thoại di động', required=True, tracking=True)
     identifier = fields.Char('Số căn cước công dân', required=True, tracking=True)
@@ -37,7 +39,6 @@ class EmployeeProfile(models.Model):
     rank_id = fields.Char(string='Cấp bậc')
     auto_create_acc = fields.Boolean(string='Tự động tạo tài khoản', default=True)
     reason = fields.Char(string='Lý Do Từ Chối')
-
     # lọc duy nhất mã nhân viên
     _sql_constraints = [
         ('employee_code_uniq', 'unique(employee_code_new)', 'Mã nhân viên phải là duy nhất!'),
@@ -59,6 +60,41 @@ class EmployeeProfile(models.Model):
     # lý do từ chối
     reason_refusal = fields.Char(string='Lý do từ chối',
                                  index=True, ondelete='restrict', tracking=True)
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+        res = super(EmployeeProfile, self).fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar,
+                                                           submenu=submenu)
+
+        # Kiểm tra xem view_type có phải là 'form' và user_id có tồn tại
+        if view_type == 'form':
+            user_id = self.env.user.id
+            # Tạo một biểu thức domain mới để xác định xem nút có nên hiển thị hay không
+            # Thuộc tính của trường phụ thuộc vào modifiers
+            res['arch'] = res['arch'].replace(
+                '<button name="action_send" string="Gửi duyệt" type="object"/>',
+                f'<button name="action_send" string="Gửi duyệt" type="object" modifiers=\'{{"invisible":["|",["state","in",["pending","approved"]],["create_uid", "!=", {user_id}]]}}\'/>'
+            )
+
+            doc = etree.XML(res['arch'])
+
+            # Truy cập và sửa đổi modifier của trường 'name' trong form view
+
+            config_group = doc.xpath("//group")  # Tìm nhóm có id là 'config'
+            if config_group:
+                cf = config_group[0]
+                for field in cf.xpath("//field[@name]"):
+                    field_name = field.get("name")
+                    if field_name not in ['phone_num', 'email', 'identifier']:
+                        modifiers = field.attrib.get('modifiers', '')
+                        modifiers = json.loads(modifiers) if modifiers else {}
+                        modifiers.update({'readonly': 1})
+                        field.attrib['modifiers'] = json.dumps(modifiers)
+
+                # Gán lại 'arch' cho res với các thay đổi mới
+            res['arch'] = etree.tostring(doc, encoding='unicode')
+
+        return res
 
     @api.depends('system_id', 'block_id')
     def render_code(self):
@@ -110,8 +146,6 @@ class EmployeeProfile(models.Model):
         # Lấy giá trị của trường related để check điều kiện hiển thị
         for record in self:
             record.related = record.block_id.name == constraint.BLOCK_OFFICE_NAME
-
-
 
     @api.onchange('company')
     def _onchange_company(self):
@@ -349,7 +383,7 @@ class EmployeeProfile(models.Model):
                     return cf
 
     def find_child_company(self, record):
-        # record là 1 hàng trong bảng cấu hình luồng phê duyệt
+        """record là 1 hàng trong bảng cấu hình luồng phê duyệt"""
         name_company_profile = self.company.name.split('.')
         if record.company:
             for comp in record.company:
@@ -362,13 +396,13 @@ class EmployeeProfile(models.Model):
         else:
             return True
 
-    # hàm này để hiển thị lịch sử lưu trữ
     def toggle_active(self):
+        """
+            Hàm này để hiển thị lịch sử lưu trữ
+        """
         for record in self:
             record.active = not record.active
             if not record.active:
                 record.message_post(body="Đã lưu trữ")
             else:
                 record.message_post(body="Bỏ lưu trữ")
-
-
