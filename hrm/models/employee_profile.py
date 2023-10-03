004b8b1eff353b09ab781fec3387db3e8907b846
Doan <doanvh@doanvh.com>
2023-10-03 09:13:00 +0700

    update quyen tao moi va chinh sua

--- hrm/models/employee_profile.py
+++ hrm/models/employee_profile.py
@@ -43,7 +43,6 @@ class EmployeeProfile(models.Model):
     auto_create_acc = fields.Boolean(string='Tự động tạo tài khoản', default=True)
     reason = fields.Char(string='Lý Do Từ Chối')
     acc_id = fields.Integer(string='Id tài khoản đăng nhập')
-
     # lọc duy nhất mã nhân viên
     _sql_constraints = [
         ('employee_code_uniq', 'unique(employee_code_new)', 'Mã nhân viên phải là duy nhất!'),
@@ -57,7 +56,15 @@ class EmployeeProfile(models.Model):
     approved_link = fields.One2many('hrm.approval.flow.profile', 'profile_id', tracking=True)
     approved_name = fields.Many2one('hrm.approval.flow.object')
 
-    _security = "hrm.hrm_group_own_edit"
+    def _can_see_all_record(self):
+        profile = self.env['hrm.employee.profile'].sudo().search([])
+        for p in profile:
+            if self.env.user.has_group('hrm.hrm_group_create_edit'):
+                p.can_see_all_record = True
+            else:
+                p.can_see_all_record = False
+
+    can_see_all_record = fields.Boolean()
 
     def _get_server_date(self):
         # Lấy ngày hiện tại theo múi giờ của máy chủ
@@ -102,10 +109,13 @@ class EmployeeProfile(models.Model):
     def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
         res = super(EmployeeProfile, self).fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar,
                                                            submenu=submenu)
+        self._can_see_all_record()
 
         # Kiểm tra xem view_type có phải là 'form' và user_id có tồn tại
-
-        if view_type == 'form' and not self.id:
+        if view_id:
+            view = self.env['ir.ui.view'].browse(view_id)
+            view_name = view.name
+        if view_type == 'form' and not self.id and view_name != 'hrm.employee.approval.form':
             user_id = self.env.user.id
             # Tạo một biểu thức domain mới để xác định xem nút có nên hiển thị hay không
             # Thuộc tính của trường phụ thuộc vào modifiers
@@ -124,9 +134,9 @@ class EmployeeProfile(models.Model):
                     if field_name != 'employee_code_new':
                         modifiers = field.attrib.get('modifiers', '')
                         modifiers = json.loads(modifiers) if modifiers else {}
-                        modifiers.update({'readonly': [["id", "!=", False]]})
+                        modifiers.update({'readonly': [["id", "!=", False],["create_uid", "!=", user_id]]})
                         if field_name in ['phone_num', 'email', 'identifier']:
-                            modifiers.update({'readonly': [["acc_id", "!=", user_id], ["id", "!=", False]]})
+                            modifiers.update({'readonly': [["acc_id", "!=", user_id], ["id", "!=", False], ["create_uid", "!=", user_id]]})
                         field.attrib['modifiers'] = json.dumps(modifiers)
 
                 # Gán lại 'arch' cho res với các thay đổi mới
@@ -451,3 +461,4 @@ class EmployeeProfile(models.Model):
                 record.message_post(body="Đã lưu trữ")
             else:
                 record.message_post(body="Bỏ lưu trữ")
+
