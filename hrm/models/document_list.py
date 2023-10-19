88322b5ca125b2103e4d932ea8e50dd639ae56a9
Doan <doanvh@doanvh.com>
2023-10-19 09:09:16 +0700

    check trùng màn danh sách tài liệu

--- hrm/models/document_list.py
+++ hrm/models/document_list.py
@@ -1,5 +1,4 @@
 import re
-
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError, AccessDenied
 from . import constraint
@@ -24,6 +23,26 @@ class DocumentListConfig(models.Model):
         for document in self.document_list:
             document.sequence = i
             i += 1
+
+    @api.constrains('name', 'block_id', 'department_id', 'position_id', 'system_id', 'company')
+    def check_duplicate_document_config(self):
+        """hàm này để kiểm tra trùng lặp cấu hình danh sách tài liệu cho các đối tượng được áp dụng"""
+        def check_exist_object(department_id=False, position_id=False, system_id=False, company=False):
+            check = self.search([('block_id', '=', self.block_id.id), ('id', 'not in', [self.id, False]),
+                                 ('department_id', '=', department_id), ('position_id', '=', position_id),
+                                 ('system_id', '=', system_id), ('company', '=', company)])
+            return check.ids
+
+        if self.position_id and check_exist_object(position_id=self.position_id.id, department_id=self.department_id.id):
+            raise ValidationError(f"Đã có cấu hình danh sách tài liệu cho vị trí {self.position_id.work_position}")
+        elif not self.position_id and self.department_id and check_exist_object(department_id=self.department_id.id):
+            raise ValidationError(f"Đã có cấu hình danh sách tài liệu cho phòng ban {self.department_id.name}")
+        elif self.company and check_exist_object(company=self.company.id, system_id=self.system_id.id):
+            raise ValidationError(f"Đã có cấu hình danh sách tài liệu cho công ty {self.company.name}")
+        elif not self.company and self.system_id and check_exist_object(system_id=self.system_id.id):
+            raise ValidationError(f"Đã có cấu hình danh sách tài liệu cho hệ thống {self.system_id.name}")
+        elif not self.system_id and not self.department_id and check_exist_object():
+            raise ValidationError(f"Đã có cấu hình danh sách tài liệu cho khối {self.block_id.name}")
 
     def unlink(self):
         for record in self:
