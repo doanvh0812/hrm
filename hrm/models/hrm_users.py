cf1265e7aca416536e0968f0d3acbc083e9aae1d
tienkmt21 <khongtien261@gmail.com>
2023-11-08 11:36:18 +0700

    update

--- hrm/models/hrm_users.py
+++ hrm/models/hrm_users.py
@@ -15,25 +15,44 @@ class Users(models.Model):
     company = fields.Many2many('hrm.companies', string='Công ty phân quyền')
     related = fields.Boolean(compute='_compute_related_')
 
-    user_block_id = fields.Many2one('hrm.blocks', string='Khối', required=True)
+    user_block_id = fields.Many2one('hrm.blocks', string='Khối', required=True,default=lambda self: self.default_block())
     user_department_id = fields.Many2one('hrm.departments', string='Phòng ban', required=True)
     user_system_id = fields.Many2one('hrm.systems', string='Hệ thống', required=True)
     user_company_id = fields.Many2one('hrm.companies', string='Công ty', required=True)
     user_code = fields.Char(string="Mã nhân viên")
-    user_position_id = fields.Many2one('hrm.position', string='Vị trí')
-    user_team_id = fields.Many2one('hrm.teams', string='Đội ngũ')
+    user_position_id = fields.Many2one('hrm.position', string='Vị trí', required=True)
+    user_team_marketing = fields.Many2one('hrm.teams', string='Đội ngũ marketing', tracking=True, domain=_default_team)
+    user_team_sales = fields.Many2one('hrm.teams', string='Đội ngũ bán hàng', tracking=True, domain=_default_team)
+    user_phone_num = fields.Char('Số điện thoại di động', required=True)
+    user_name_display = fields.Char('Tên hiển thị', required=True, readonly=True)
+    user_related = fields.Boolean(compute='compute_related')
+
+    def default_block(self):
+        return self.env['hrm.blocks'].sudo().search([('name', '=', constraint.BLOCK_COMMERCE_NAME)])
 
     @api.depends('block_id')
     def _compute_related_(self):
         # Lấy giá trị của trường related để check điều kiện hiển thị
         for record in self:
-            if not record.block_id:
-                record.block_id = 'full'
             record.related = record.block_id == constraint.BLOCK_OFFICE_NAME
+
+    @api.depends('user_block_id')
+    def compute_related(self):
+        # Lấy giá trị của trường related để check điều kiện hiển thị
+        for record in self:
+            record.user_related = record.user_block_id.name == constraint.BLOCK_OFFICE_NAME
 
     @api.onchange('block_id')
     def _onchange_block_id(self):
         self.department_id = self.system_id = self.company = False
+
+    @api.onchange('user_block_id')
+    def _onchange_block_id(self):
+        """
+            Khi chọn lại khối clear hết data cũ đã nhập
+        """
+        self.user_position_id = self.user_system_id = self.user_company_id = self.user_department_id \
+            = self.user_team_sales = self.user_team_marketing = False
 
     @api.onchange('system_id')
     def _onchange_system_id(self):
